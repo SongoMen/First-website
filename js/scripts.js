39c5a5c3a483cb3313120f06e2ebc29edbcedd9b
SongoMen <songomengaming@gmail.com>
2018-03-03 02:51:26 +0100

    to do

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 setTimeout(function() {
     $('#loading').delay(1200).fadeOut('slow');
 }, 1000);
+
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 200) {
+            header.removeClass('header').addClass("black");
+        } else {
+            header.removeClass("black").addClass('header');
+        }
+    });
+});
