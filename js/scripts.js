3ea04b5e27bf808a528664ad5f6155112a488f7c
SongoMen <songomengaming@gmail.com>
2018-03-03 16:22:34 +0100

    s

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,9 @@ setTimeout(function() {
     $('#loading').delay(1200).fadeOut('slow');
 }, 1000);
 
-$(function() {
-    //caches a jQuery object containing the header element
-    var header = $(".header");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
 
-        if (scroll >= 200) {
-            header.removeClass('header').addClass("black");
-        } else {
-            header.removeClass("black").addClass('header');
-        }
-    });
-});
+today = dd + '/' + mm + '/' + yyyy;
