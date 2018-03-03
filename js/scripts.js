4cc7a85e2f0f7da6607c43362c85124c43efe3f1
SongoMen <songomengaming@gmail.com>
2018-03-03 01:45:19 +0100

    l

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
 setTimeout(function() {
     $('#loading').delay(1200).fadeOut('slow');
 }, 1000);
-
-window.onscroll = function() {menuheader()};
-
-function menuheader() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementsByClassName(".header").className = "bar";
-    } else {
-        document.getElementsByClassName(".header").className = "";
-    }
-}
-
--- scripts.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function() {
-    $('#mydiv').fadeOut('fast');
-}, 1000);
