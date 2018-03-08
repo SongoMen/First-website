90a663a0a1bb05108dc6e664b99545c8e68fda94
SongoMen <pat.kozlowski2000@onet.pl>
2018-03-08 02:09:10 +0100

    Button

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,10 @@
-var $animation_elements = $('.home1');
-var $window = $(window);
-
-$window.on('scroll', check_if_in_view);
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
-
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
-
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
+jQuery(document).ready(function(){
+  jQuery('#dalej').on('click', function(event) {        
+       jQuery('#home').toggle('hide');
+       jQuery('#article2').toggle('show');
+       $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
   });
-}
+});
 
 var today = new Date();
 var day = today.getDate();
