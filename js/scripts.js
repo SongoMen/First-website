c28f7323999b061e9b5af1468658522d21f8f5a6
SongoMen <pat.kozlowski2000@onet.pl>
2018-03-10 02:40:57 +0100

    anim

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,21 @@ jQuery(document).ready(function(){
     if($('#home').is(':visible')) {        
        jQuery('#home').toggle('hide');
        jQuery('#article2').toggle('show');
+       jQuery('#opt1').toggle('isActive');
     }
+
     
        $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
   });
+  if($('#home').is(':visible')) {   
+    $("#opt1").animate({width: 100}, 800 );
+    jQuery('#opt1').addClass('isActive');
+    
+  }
+  if($('#article2').is(':visible')) {   
+    $("#opt2").animate({width: 100}, 800 );
+    jQuery('#opt2').addClass('isActive');
+  }
 });
 
 var today = new Date();
