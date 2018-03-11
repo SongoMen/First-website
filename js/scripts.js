91aaa25bc2932c2b3998458eddf87b4ff8764586
SongoMen <pat.kozlowski2000@onet.pl>
2018-03-11 15:00:29 +0100

    l

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,52 @@
 jQuery(document).ready(function(){
   jQuery('#dalej').on('click', function(event) {
-    if($('#home').is(':visible')) {        
-       jQuery('#home').toggle('hide');
-       jQuery('#article2').toggle('show');
-       jQuery('#opt1').toggle('isActive');
-       $("#opt2").animate({width: 100}, 800 );
-       jQuery('#opt2').addClass('isActive');
-    }  
-  $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
+    jQuery('#home').toggle('hide');
+    jQuery('#article2').toggle('show');
+    jQuery('#opt1').toggle('isActive');
+    $("#opt2").animate({width: 100}, 800 );
+    jQuery('#opt2').addClass('isActive');
+    $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
   });
+
+  jQuery('#dalej2').on('click', function(event) {
+    jQuery('#article2').toggle('hide');
+    jQuery('#article3').toggle('show');
+    jQuery('#opt2').toggle('isActive');
+    $("#opt3").animate({width: 100}, 800 );
+    jQuery('#opt3').addClass('isActive');
+    $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
+  });
+
+  jQuery('#dalej3').on('click', function(event) {
+    jQuery('#article3').toggle('hide');
+    jQuery('#article4').toggle('show');
+    jQuery('#opt3').toggle('isActive');
+    $("#opt4").animate({width: 100}, 800 );
+    jQuery('#opt4').addClass('isActive');
+    $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
+  });
+
+  jQuery('#dalej4').on('click', function(event) {
+    jQuery('#article4').toggle('hide');
+    jQuery('#article5').toggle('show');
+    jQuery('#opt4').toggle('isActive');
+    $("#opt5").animate({width: 100}, 800 );
+    jQuery('#opt5').addClass('isActive');
+    $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
+  });
+
+  jQuery('#dalej5').on('click', function(event) {
+    jQuery('#article5').toggle('hide');
+    jQuery('#article1').toggle('show');
+    jQuery('#opt5').toggle('isActive');
+    $("#trans").animate( { "opacity": "show", top:"100"} , 500 );
+  });
+
   if($('#home').is(':visible')) {   
     $("#opt1").animate({width: 100}, 800 );
     jQuery('#opt1').addClass('isActive');
   }
   
-  if($('#article2').is(':visible')) {   
-    $("#opt2").animate({width: 100}, 800 );
-    jQuery('#opt2').addClass('isActive');
-  }
 });
 
 var today = new Date();
